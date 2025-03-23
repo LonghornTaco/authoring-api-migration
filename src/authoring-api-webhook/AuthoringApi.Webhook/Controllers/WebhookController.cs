2b7c146621219465b4f7188f9316c0e7b5d389b7
longhorntaco <jasonmwilkerson@hotmail.com>
2025-03-23 17:10:58 -0500

    updates

--- src/authoring-api-webhook/AuthoringApi.Webhook/Controllers/WebhookController.cs
+++ src/authoring-api-webhook/AuthoringApi.Webhook/Controllers/WebhookController.cs
@@ -48,3 +48,5 @@ namespace AuthoringApi.Webhook.Controllers
         }
     }
 }
+
+
---
+++ src/authoring-api-xm/AuthoringApiXm.Platform/Events/MyItemSavedEvent.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Abstractions;
+using Sitecore.Data.Items;
+using Sitecore.Events;
+using System;
+
+namespace AuthoringApiXm.Platform.Events
+{
+    public class MyItemSavedEvent
+    {
+        private readonly BaseLog _log;
+
+        public MyItemSavedEvent(BaseLog log)
+        {
+            _log = log ?? throw new ArgumentNullException(nameof(log));
+        }
+
+        public void OnItemSaved(object sender, EventArgs args)
+        {
+            var item = Event.ExtractParameter(args, 0) as Item;
+            var templateId = item.TemplateID.ToGuid();
+
+            if (templateId != Guid.Empty)
+            {
+                _log.Info("LonghornTaco is COOL!", this);
+            }
+        }
+    }
+}
+
+
